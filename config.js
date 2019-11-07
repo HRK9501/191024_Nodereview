b1bf603b8ca5240ea7b81ebc7ec2c3b5b4f98ef7
hrk9501 <h_kim90825@fanshaweonline.ca>
2019-11-07 17:33:31 -0500

    error page

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     host: "localhost",
     port: 3306, // 8889 for mac
     uname: "root",
-    pword: "root", // leave blank for windows
+    pword: "", // leave blank for windows
     database: "db_card"
 }
 
